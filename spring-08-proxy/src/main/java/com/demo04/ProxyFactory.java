68d772aa7e05740846b9ba8db8732ee91b850ae5
langli <langli@imdada.cn>
2021-02-25 19:46:55 +0800

    动态代理

--- spring-08-proxy/src/main/java/com/demo04/ProxyFactory.java
+++ spring-08-proxy/src/main/java/com/demo04/ProxyFactory.java
@@ -8,6 +8,11 @@ import java.lang.reflect.Method;
 
 
 public class ProxyFactory implements MethodInterceptor {
+    //该代理类使用cglib实现
+    /*
+    1、动态代理必须实现InvocationHandler接口，通过反射代理方法，比较消耗系统性能，但可以减少代理类的数量，使用更灵活。
+    2、cglib代理无需实现接口，通过生成类字节码实现代理，比反射稍快，不存在性能问题，但cglib会继承目标对象，需要重写方法，所以目标对象不能为final类。
+     */
     private Object target;//维护一个目标对象
 
     public ProxyFactory(Object target) {
